5bd8c658522bdc8c63d0ad522faa3c8349a949f0
sapinheiro <sampinheiro9@gmail.com>
2019-07-02 18:43:04 -0400

    Added work experience, modifying css

--- personal-website/src/sections/experience.jsx
+++ personal-website/src/sections/experience.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 
 class Experience extends Component {
@@ -6,7 +6,79 @@ class Experience extends Component {
         return (
             <section id="experience">
                 <div>
-
+                    <div className="education row">
+                        <div className="four columns header-col">
+                            <h1><span>Full-Time Experience</span></h1>
+                        </div>
+                        <div className="nine columns main-col">
+                            <div className="row item">
+                                <div className="twelve columns">
+                                    <h3>Schonfeld Strategic Advisors</h3>
+                                    <p className="info">
+                                        Programmer / Data Analyst
+                                        <br/>
+                                        <em className="date">July 2018 - December 2018</em>
+                                    </p>
+                                    <p>
+                                        <span>&bull;</span> Developed primarily on projects involving data processing,
+                                        visualization, and analysis<br/>
+                                        <span>&bull;</span> Fulfilled data science duties for team primarily involving
+                                        SQL, Q, and Excel<br/>
+                                        <span>&bull;</span> Updated company data as well as streamlining processes for
+                                        unmanageable legacy code<br/>
+                                        <span>&bull;</span> Collaborated with team on principal projects under strict
+                                        deadlines
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="row item">
+                                <div className="twelve columns">
+                                    <h3>Charles River Development</h3>
+                                    <p className="info">
+                                        Software Engineer
+                                        <br/>
+                                        <em className="date">January 2017 - June 2017</em>
+                                    </p>
+                                    <p>
+                                        <span>&bull;</span> Wrote software enchancements and fixed bugs through
+                                        Java/C#/SQL <br/>
+                                        <span>&bull;</span> Created, executed, and monitored Java unit and automation
+                                        tests within framework<br/>
+                                        <span>&bull;</span> Validated system enhancements, fixes, and user
+                                        workflows<br/>
+                                        <span>&bull;</span> Expedited employee workflows with utilities<br/>
+                                        <span>&bull;</span> Maintained development and test environments by upgrading
+                                        databases and servers<br/>
+                                        <span>&bull;</span> Gained experience in a number of financial areas namely
+                                        trade order management
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="four columns header-col">
+                            <h1><span>Part-Time Experience</span></h1>
+                        </div>
+                        <div className="nine columns main-col">
+                            <div className="row item">
+                                <div className="twelve columns">
+                                    <h3>Khoury College of Computer Sciences</h3>
+                                    <p className="info">
+                                        Teaching Assistant
+                                        <br/>
+                                        <em className="date">January 2017 - July 2019</em>
+                                    </p>
+                                    <p>
+                                        <span>&bull;</span> Taught fellow students basic Java, higher-level
+                                        Object-Oriented Design, and teamwork<br/>
+                                        <span>&bull;</span> Nurtured their understanding of course concepts<br/>
+                                        <span>&bull;</span> Graded homeworks, tests, and quizzes
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </section>
         );
--- personal-website/src/sections/projects.jsx
+++ personal-website/src/sections/projects.jsx
@@ -5,15 +5,16 @@ class Projects extends Component {
     render() {
         return (
             <section id="projects">
-                <h2>PROJECTS</h2>
+                <h2>Projects</h2>
+                <h1>*Note: Some of my projects on github were requested by those I worked with to be private. Can be made public upon request.</h1>
                 <div className="row">
-                    <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-                        <div className="columns collapsed portfolio-item">
+                    <div id="projects-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+                        <div className="columns collapsed projects-item">
                             <div className="item-wrap">
                                 <a href="#modal-01">
                                     <img src="/img/website.jpg" className="item-img"/>
                                     <div className="overlay">
-                                        <div className="portfolio-item-meta">
+                                        <div className="projects-item-meta">
                                             <h5>Personal Website (React)</h5>
                                             <p>This is the second iteration of my personal website. First version used basic html and css, while my primary focus on this version was to develop through React.</p>
                                         </div>
@@ -21,12 +22,12 @@ class Projects extends Component {
                                 </a>
                             </div>
                         </div>
-                        <div className="columns collapsed portfolio-item">
+                        <div className="columns collapsed projects-item">
                             <div className="item-wrap">
                                 <a href="#modal-01">
                                     <img src="/img/adventure.jpg" className="item-img"/>
                                     <div className="overlay">
-                                        <div className="portfolio-item-meta">
+                                        <div className="projects-item-meta">
                                             <h5>Adventure Game (Python Engine)</h5>
                                             <p>I'm undertaking the totality of the coding and script-writing process for the development of a game. Freelancing for the art and other assets.</p>
                                         </div>
@@ -34,12 +35,12 @@ class Projects extends Component {
                                 </a>
                             </div>
                         </div>
-                        <div className="columns collapsed portfolio-item">
+                        <div className="columns collapsed projects-item">
                             <div className="item-wrap">
                                 <a href="#modal-01">
                                     <img src="/img/bnb.jpg" className="item-img"/>
                                     <div className="overlay">
-                                        <div className="portfolio-item-meta">
+                                        <div className="projects-item-meta">
                                             <h5>Bits & Bots</h5>
                                             <p>I develop open source curriculum and taught beginner robotics lessons in
                                                 lower-income areas of Boston. Our goal is to promote an interest in STEM
@@ -51,12 +52,12 @@ class Projects extends Component {
                                 </a>
                             </div>
                         </div>
-                        <div className="columns collapsed portfolio-item">
+                        <div className="columns collapsed projects-item">
                             <div className="item-wrap">
                                 <a href="#modal-01">
                                     <img src="/img/tetris.jpg" className="item-img"/>
                                     <div className="overlay">
-                                        <div className="portfolio-item-meta">
+                                        <div className="projects-item-meta">
                                             <h5>Tetris Game (Processing)</h5>
                                             <p>Developed a variation of a Tetris game that utilized Decision Tree
                                                 Learning to assign various "personalities" to the pieces as the game
@@ -69,13 +70,13 @@ class Projects extends Component {
                     </div>
                 </div>
                 <div className="row">
-                    <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-                        <div className="columns collapsed portfolio-item">
+                    <div id="projects-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+                        <div className="columns collapsed projects-item">
                             <div className="item-wrap">
                                 <a href="#modal-01">
                                     <img src="/img/music.jpg" className="item-img"/>
                                     <div className="overlay">
-                                        <div className="portfolio-item-meta">
+                                        <div className="projects-item-meta">
                                             <h5>Music Editor (Java)</h5>
                                             <p>A music editor made using the Model-View-Controller pattern and object-oriented practices.</p>
                                         </div>
@@ -83,12 +84,12 @@ class Projects extends Component {
                                 </a>
                             </div>
                         </div>
-                        <div className="columns collapsed portfolio-item">
+                        <div className="columns collapsed projects-item">
                             <div className="item-wrap">
                                 <a href="#modal-01">
                                     <img src="/img/dogs.jpg" className="item-img"/>
                                     <div className="overlay">
-                                        <div className="portfolio-item-meta">
+                                        <div className="projects-item-meta">
                                             <h5>Animal Shelter Website (React)</h5>
                                             <p>Sample website created to learn React. Can interact with a database and deals with several CRUD permissions.</p>
                                         </div>
@@ -96,12 +97,12 @@ class Projects extends Component {
                                 </a>
                             </div>
                         </div>
-                        <div className="columns collapsed portfolio-item">
+                        <div className="columns collapsed projects-item">
                             <div className="item-wrap">
                                 <a href="#modal-01">
                                     <img src="/img/shell.png" className="item-img"/>
                                     <div className="overlay">
-                                        <div className="portfolio-item-meta">
+                                        <div className="projects-item-meta">
                                             <h5>Unix Shell (C)</h5>
                                             <p>A bash implementation that supports command line operations like piping and redirects.</p>
                                         </div>
@@ -109,12 +110,12 @@ class Projects extends Component {
                                 </a>
                             </div>
                         </div>
-                        <div className="columns collapsed portfolio-item">
+                        <div className="columns collapsed projects-item">
                             <div className="item-wrap">
                                 <a href="#modal-01">
                                     <img src="/img/pension.jpg" className="item-img"/>
                                     <div className="overlay">
-                                        <div className="portfolio-item-meta">
+                                        <div className="projects-item-meta">
                                             <h5>Pension Calculator</h5>
                                             <p>An updated retirement calculator made for the MSRB. Main goal was to simplify the experience for the user as much as possible. Worked on both the front-end and back-end.</p>
                                         </div>
@@ -125,13 +126,13 @@ class Projects extends Component {
                     </div>
                 </div>
                 <div className="row">
-                    <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-                        <div className="columns collapsed portfolio-item">
+                    <div id="projects-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+                        <div className="columns collapsed projects-item">
                             <div className="item-wrap">
                                 <a href="#modal-01">
                                     <img src="/img/raft.jpg" className="item-img"/>
                                     <div className="overlay">
-                                        <div className="portfolio-item-meta">
+                                        <div className="projects-item-meta">
                                             <h5>Raft Consensus Algorithm (Python)</h5>
                                             <p>Implemented a version of the <a href="https://raft.github.io/">Raft Protocol</a> that still works in lossy and unreliable networks.</p>
                                         </div>
@@ -139,12 +140,12 @@ class Projects extends Component {
                                 </a>
                             </div>
                         </div>
-                        <div className="columns collapsed portfolio-item">
+                        <div className="columns collapsed projects-item">
                             <div className="item-wrap">
                                 <a href="#modal-01">
                                     <img src="/img/memory.jpg" className="item-img"/>
                                     <div className="overlay">
-                                        <div className="portfolio-item-meta">
+                                        <div className="projects-item-meta">
                                             <h5>Dynamic Memory Allocator (C)</h5>
                                             <p>A dynamic and fast memory allocator to emulate the behavior of malloc in the C library.</p>
                                         </div>
@@ -152,12 +153,12 @@ class Projects extends Component {
                                 </a>
                             </div>
                         </div>
-                        <div className="columns collapsed portfolio-item">
+                        <div className="columns collapsed projects-item">
                             <div className="item-wrap">
                                 <a href="#modal-01">
                                     <img src="/img/aquarium.jpg" className="item-img"/>
                                     <div className="overlay">
-                                        <div className="portfolio-item-meta">
+                                        <div className="projects-item-meta">
                                             <h5>Aquarium Simulator (SQL/Ruby on Rails)</h5>
                                             <p>Simulates data interaction of a mock aquarium. Allows a user to manipulate the data and learn about the relationships between the various interconnecting entities.</p>
                                         </div>
@@ -165,12 +166,12 @@ class Projects extends Component {
                                 </a>
                             </div>
                         </div>
-                        <div className="columns collapsed portfolio-item">
+                        <div className="columns collapsed projects-item">
                             <div className="item-wrap">
                                 <a href="#modal-01">
                                     <img src="/img/file.jpg" className="item-img"/>
                                     <div className="overlay">
-                                        <div className="portfolio-item-meta">
+                                        <div className="projects-item-meta">
                                             <h5>Fuse File System (C)</h5>
                                             <p>A file system created from FUSE API. Can create files, directories, hard links, and perform simple file system commands.</p>
                                         </div>
@@ -180,7 +181,6 @@ class Projects extends Component {
                         </div>
                     </div>
                 </div>
-                <p>*Note: Some of my projects on github were requested by those I worked with to be private. Can be made public upon request.</p>
             </section>
         );
     }
