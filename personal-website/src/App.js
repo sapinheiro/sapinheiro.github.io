77cc2238edd32389deecfba57d2aae85a0bd8ba8
sapinheiro <sampinheiro9@gmail.com>
2019-07-01 22:21:20 -0400

    Moved some projects over as well as beginning to use new bootstrap template

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
-import Header from './components/header'
-import About from './components/about'
-import Projects from './components/projects'
-import Education from './components/education'
-import Experience from './components/experience'
-import Skills from './components/skills'
-
+import Header from './sections/header'
+import Projects from './sections/projects'
+import Education from './sections/education'
+import Experience from './sections/experience'
+import Skills from './sections/skills'
 
 class App extends Component {
-    render() {
-        return (
-            <div id="colorlib-page">
-                <div id="container-wrap">
-                    <Header></Header>
-                    <div id="colorlib-main">
-                        <Projects></Projects>
-                        <Education></Education>
-                        <Experience></Experience>
-                        <Skills></Skills>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  render() {
+      return (
+          <div id="colorlib-page">
+              <div id="container-wrap">
+                  <Header/>
+                  <div id="colorlib-main">
+                      <Projects/>
+                      <Education/>
+                      <Experience/>
+                      <Skills/>
+                  </div>
+              </div>
+          </div>
+      );
+  }
 }
 
 export default App;
