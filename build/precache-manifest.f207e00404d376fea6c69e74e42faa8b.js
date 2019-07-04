cd20d42edd69a53b7f47b0331e62d2162c6c1d57
sapinheiro <sampinheiro9@gmail.com>
2019-07-04 17:27:36 -0400

    Changing the way they read directory structure

---
+++ build/precache-manifest.f207e00404d376fea6c69e74e42faa8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "77d44184e58a414d6546",
+    "url": "sampinheiro.com/static/js/runtime~main.77d44184.js"
+  },
+  {
+    "revision": "c4c343f62d91c63a4564",
+    "url": "sampinheiro.com/static/js/main.c4c343f6.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "sampinheiro.com/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "c4c343f62d91c63a4564",
+    "url": "sampinheiro.com/static/css/main.a2c77d10.chunk.css"
+  },
+  {
+    "revision": "142dd52b692e3af8f4ca0e74460f2226",
+    "url": "sampinheiro.com/index.html"
+  }
+];
